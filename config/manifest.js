45d8397bc168761d7c137929a4c8283fda5a3749
Kyle Coberly <kyle.coberly@gmail.com>
2017-03-28 14:38:46 -0700

    Adds basic landing page

---
+++ config/manifest.js
@@ -0,0 +1,19 @@
+/*jshint node:true*/
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+	// See https://github.com/san650/ember-web-app#documentation for a list of
+	// supported properties
+
+	return {
+		name: "Ceres Staff Management",
+		short_name: "Ceres",
+		description: "Staff management tool",
+		start_url: "/",
+		display: "standalone",
+		background_color: "83b0e2",
+		theme_color: "#333",
+		icons: [
+		]
+	};
+}

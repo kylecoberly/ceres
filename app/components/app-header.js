cc0a024610b538a361c2012249da8c9d642094e1
Kyle Coberly <kyle.coberly@gmail.com>
2017-03-29 10:28:05 -0700

    Cleans up code

---
+++ app/components/app-header.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	tagName: "header",
+	classNames: ["app-header"],
+	classNameBindings: ["mobileHeader"],
+	mobileHeader: Ember.computed.alias("media.isMobile")
+});

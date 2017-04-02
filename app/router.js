9d29668fd7bfc3311e1d9e704f605f8bfdbde565
Kyle Coberly <kyle.coberly@gmail.com>
2017-04-02 12:18:58 -0600

    Adds schedule page

--- app/router.js
+++ app/router.js
@@ -2,14 +2,15 @@ import Ember from "ember";
 import config from "./config/environment";
 
 const Router = Ember.Router.extend({
-    location: config.locationType,
-    rootURL: config.rootURL
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
 Router.map(function(){
-  this.route("class-metrics");
-  this.route("tools");
-  this.route("mission");
+	this.route("class-metrics");
+	this.route("tools");
+	this.route("mission");
+	this.route("schedules");
 });
 
 export default Router;

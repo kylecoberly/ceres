18aadae6c2cc48ef7d29a1a35e906182563e629a
Kyle Coberly <kyle.coberly@gmail.com>
2017-04-02 13:53:01 -0600

    Moved model back to route

--- app/routes/schedules.js
+++ app/routes/schedules.js
@@ -1,4 +1,85 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(){
+		return {
+			workshopSchedules: [{
+				id: 1,
+				workshopName: "JavaScript Fundamentals",
+				weeks: [{
+					label: "Week 1",
+					days: [{
+						date: "3/14",
+						instructor: "Berto"
+					},{
+						date: "3/16",
+						instructor: "Berto"
+					}]
+				},{
+					label: "Week 2",
+					days: [{
+						date: "3/21",
+						instructor: "Danny"
+					},{
+						date: "3/23",
+						instructor: "Isaac"
+					}]
+				},{
+					label: "Week 3",
+					days: [{
+						date: "3/28",
+						instructor: "Peter"
+					},{
+						date: "3/30",
+						instructor: "Peter"
+					}]
+				},{
+					label: "Week 4",
+					days: [{
+						date: "4/4",
+						instructor: "Peter"
+					},{
+						date: "4/6",
+						instructor: "Peter"
+					}]
+				},{
+					label: "Week 5",
+					days: [{
+						date: "4/11",
+						instructor: "Peter"
+					},{
+						date: "4/13",
+						instructor: "Peter"
+					}]
+				},{
+					label: "Week 6",
+					days: [{
+						date: "4/18",
+						instructor: "Peter"
+					},{
+						date: "4/20",
+						instructor: "Peter"
+					}]
+				},{
+					label: "Week 7",
+					days: [{
+						date: "4/25",
+						instructor: "Peter"
+					},{
+						date: "4/27",
+						instructor: "Peter"
+					}]
+				},{
+					label: "Week 8",
+					days: [{
+						date: "5/2",
+						instructor: "Peter"
+					},{
+						date: "5/4",
+						instructor: "Peter"
+					}]
+				}]
+			}]
+		};
+	}
 });
